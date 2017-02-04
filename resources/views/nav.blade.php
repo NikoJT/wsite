02c7ec3d4c1869439f3b58a90630e942e83e587b
R3pulse <niko.tarmaa@gmail.com>
2017-02-04 18:15:47 +0200

    First commit

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,25 @@
+<nav id="primary-menu">
+  <ul>
+    <li><a href="index.html">
+        <div>Home</div></a></li>
+    <li><a href="#">
+        <div>Features</div></a></li>
+    <li><a href="MyPages">
+        <div>MyPages</div></a></li>
+    <li class="mega-menu"><a href="#">
+        <div>Portfolio</div></a></li>
+    <li class="mega-menu"><a href="#">
+        <div>Blog</div></a></li>
+    <li><a href="users.html">
+        <div>Users</div></a></li>
+    <li class="mega-menu"><a href="#">
+        <div>Shortcodes</div></a></li>
+  </ul>
+  <!-- Top Search-->
+  <div id="top-search"><a id="top-search-trigger" href="#"><i class="icon-search3"></i><i class="icon-line-cross"></i></a>
+    <form action="search.html" method="get">
+      <input type="text" name="q" value="" placeholder="Type &amp; Hit Enter.." class="form-control"/>
+    </form>
+  </div>
+  <!-- #top-search end-->
+</nav>
